d996d525e688dd7e6ccaf583025e0c8b01ff686f
Muhammad Asif <115541626+asifbinfarooq1@users.noreply.github.com>
2022-11-12 17:40:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function qoute() {
+    let a = document.getElementById("inp").value;
+    const encodedParams = new URLSearchParams();
+encodedParams.append("q", a);
+encodedParams.append("target", "ur");
+encodedParams.append("source", "en");
+
+const options = {
+	method: 'POST',
+	headers: {
+		'content-type': 'application/x-www-form-urlencoded',
+		'Accept-Encoding': 'application/gzip',
+		'X-RapidAPI-Key': '12f7f36fbbmshb5384e520e260dap1ac6c7jsn84c2d2f84776',
+		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+	},
+	body: encodedParams
+};
+
+fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
+	.then(response => response.json())
+	.then(data => { 
+        console.log(data)
+
+       document.getElementById("result").innerHTML =`${data.data.translations[0].translatedText}`
+    })
+	.catch(err => console.error(err));
+
+}
